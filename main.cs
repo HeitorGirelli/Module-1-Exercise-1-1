95fbf491e8604493b2f1708f54da53ef68326472
Heitor Girelli <heitor.girelli@hotmail.com>
2024-05-26 14:39:30 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,11 @@
+using static System.Console;
+// This program displays a person’s name, birthdate, work phone number, and cell phone number on separate lines.
+class Program {
+  public static void Main () {
+    // Unit Test #1 
+    WriteLine ("Lucinda Potter"); // Displays Name
+    WriteLine ("6/24/1992"); // Displays Birthdate
+    WriteLine ("Work 000-000-0101"); // Displays Work Phone Number
+    WriteLine ("Cell 000-000-0189"); // Displays Cell Phone Number
+  } // End of Class
+} // End of Program
